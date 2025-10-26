8010f2a2df38d1643de95d0af2228a3cb4db1a24
זלקינד רבקה <1627795@main.local>
2025-10-26 15:02:44 +0200

    changes of lesson 4

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { USERS } from '../fake_users';
 
 @Component({
   selector: 'app-user',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './user.css'
 })
 export class User {
+  selectedUser = USERS[0];
 
+  get userImgPath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  
+  changeUser(){
+    const randomIndex = Math.floor(Math.random() * USERS.length);
+    this.selectedUser = USERS[randomIndex];
+  }
 }
